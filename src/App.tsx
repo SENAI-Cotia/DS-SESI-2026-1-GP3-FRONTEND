621ad17d6c26b94e2db43057da495966cb8f6ecc
Miguel Francelino <miguelfrancellino1705@gmail.com>
2026-04-22 14:44:23 -0300

    Criação visual da navbar (Acho que não aguento mais)

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@
 // Outras dependências olhar no app
 // npm run dev
 
+import Navbar from "./components/navbar"
+
 function App() {
- return(
- <div className="w-full flex justify-center">
-   <h1 className="text-red-700 text-5xl">Hello World</h1>
- </div>
- )
+  return (
+    <>
+      <Navbar />
+
+      <div>
+
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,40 @@
+import logo from "../assets/KOR-logo.png"
+import { Search, UserCircle, UserRound, } from "lucide-react"
+
+export default function Navbar() {
+    return (
+        <div className="bg-[#3E579D] text-white font-extralight p-8 h-24 flex items-center justify-between" style={{ fontFamily: "Inter, sans-serif", fontWeight: 300 }}>
+
+
+            {/* logo */}
+            <div className="flex items-center cursor-pointer">
+                <img src={logo} alt="KOR logo" className="h-16 invert" />
+                <p className="text-white p-4 font-light">Kingdom of Reading</p>
+            </div>
+
+
+            {/* interações e pesquisa */}
+            <div className="flex gap-12 items-center ">
+                <a href="" className="hover:scale-110 transition">Início</a>
+                <a href="" className="hover:scale-110 transition">Sobre</a>
+                <a href="" className="hover:scale-110 transition">Contato</a>
+
+                <div className="flex items-center bg-white rounded-full px-4 py-1.5 w-80 border border-gray-300 focus-within:ring-1 focus-within:ring-blue-400" >
+
+                    <Search className="text-gray-500 mr-2" size={18}></Search>
+
+                    <input
+                    type="text"
+                    placeholder="Buscar..."
+                    className="w-full text-black outline-none">
+                    </input>
+                </div>
+
+                <UserRound  className="text-white cursor-pointer hover:scale-110 transition" size={36} />
+
+
+            </div>
+        </div>
+
+    )
+}
