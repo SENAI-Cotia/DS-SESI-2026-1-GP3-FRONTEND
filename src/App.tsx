28fb07c6a8c977c71e4bd6da6a9ee804c54b68eb
Miguel Francelino <miguelfrancellino1705@gmail.com>
2026-04-22 11:18:43 -0300

    primeiras configs do vite e tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// npm create vite@latest
+// npm install tailwindcss @tailwindcss/vite
+// Outras dependências olhar no app
+// npm run dev
+
+function App() {
+ return(
+ <div className="w-full flex justify-center">
+   <h1 className="text-red-700 text-5xl">Hello World</h1>
+ </div>
+ )
+}
+
+export default App
